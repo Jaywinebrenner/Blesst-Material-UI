ff5a80d1d9ba9d1e2f376d0b72ca975de88bb944
Jay Winebrenner <jaywinebrenner@gmail.com>
2020-03-18 20:14:07 -0700

    add left pane

--- src/components/Exercises/index.js
+++ src/components/Exercises/index.js
@@ -1,36 +1,62 @@
-import React from 'react';
-import { Grid, Paper, Typography } from '@material-ui/core';
+
+import React, { Fragment } from 'react'
+import { Grid, Paper, AppBar, Button, Toolbar, Typography, List, ListItem, ListItemIcon, ListItemText, Divider} from '@material-ui/core';
+
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+
+
+
 
 
 const styles = {
+  Paper: {
+    padding: 20,
 
-  Paper: {padding: 20, marginTop: 10, marginBottom: 10}
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 5,
+    marginRight: 5,
+  }
 }
-
 
 export default ({ exercises }) =>
 
-<div>
-<Grid container sm={12}>
- <Grid  item sm={6}>
-   <Paper style={styles.Paper}>
-   {exercises.map(([group, exercises]) =>
-     <Typography
-       variant="h4"
-       style={{textTranform: 'capitalize'}}
-       >
-       {group}
-      </Typography>
-   )}
-   </Paper>
- </Grid>
+  <Grid container>
+    <Grid item sm>
+      <Paper style={styles.Paper}>
+        {exercises.map(([group, exercises]) =>
+          <Fragment>
+            <Typography
+              variant="headline"
+              style={{ textTransform: 'capitalize' }}
+            >
+              {group}
+            </Typography>
 
+            <List component="ul">
+              {exercises.map(({ title }) =>
+              <ListItem button component='a' href='#simple-list'>
+                <ListItemText primary={title} />
+              </ListItem>
+            )}
 
+            </List>
 
-  <Grid item sm={6}>
-    <Paper style={styles.Paper}>
-    Left Pane
-    </Paper>
+            </Fragment>
+              )}
+      </Paper>
     </Grid>
-</Grid>
-</div>
+    <Grid item sm>
+      <Paper style={styles.Paper}>
+        <Typography
+          variant='h5'>
+          Welcome to Blesst Chest
+        </Typography>
+        <Typography
+          variant='h3'>
+          Please select a music
+        </Typography>
+      </Paper>
+    </Grid>
+  </Grid>
--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import {AppBar, Button, Toolbar, Typography, List, ListItem, ListItemIcon, ListItemText, Divider} from '@material-ui/core';
 
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
 
 const Header = () => {
 
@@ -15,29 +13,7 @@ const Header = () => {
       <Typography variant="h3" color='inherit' align='right'>
         Blesst Chest
       </Typography>
-      <List component="nav" aria-label="main mailbox folders">
-  <ListItem button>
-    <ListItemIcon>
-      <InboxIcon />
-    </ListItemIcon>
-    <ListItemText primary="Inbox" />
-  </ListItem>
-  <ListItem button>
-    <ListItemIcon>
-      <DraftsIcon />
-    </ListItemIcon>
-    <ListItemText primary="Drafts" />
-  </ListItem>
-</List>
-<Divider />
-<List component="nav" aria-label="secondary mailbox folders">
-  <ListItem button>
-    <ListItemText primary="Trash" />
-  </ListItem>
 
-    <ListItemText primary="Spam" />
-
-</List>
 
 
       <Button color="inherit">Login</Button>
